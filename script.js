23f4ae87872fe50f0861de18f2e95dad1f9ace79
Lilith-debug <lilith.nil@mailfence.com>
2022-06-07 16:50:34 -0300

    Store numbers in array and make clear button work

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function add(a,b) {
 }
 
 function substract(a,b) {
-    return a - b:
+    return a - b;
 }
 
 function multiply(a,b) {
@@ -25,3 +25,107 @@ function operate (a, operator, b) {
         return divide(a,b);
     }
 }
+
+let operation = [];
+let number = [];
+let result = null;
+
+function storeNumber (newNumber) {
+    number.push(newNumber);
+    console.log(number);
+}
+
+//function storeOperator (newOperator) {
+//    if (operator === null) {
+//        operator = newOperator;
+//    }
+//}
+
+const displayText = document.querySelector("#displayText");
+
+const one = document.querySelector("#one");
+one.addEventListener('click', () => {
+    storeNumber(1);
+});
+
+const two = document.querySelector("#two");
+two.addEventListener('click', () => {
+    storeNumber(2);
+});
+
+const three = document.querySelector("#three");
+three.addEventListener('click', () => {
+    storeNumber(3);
+});
+
+const four = document.querySelector("#four");
+four.addEventListener('click', () => {
+    storeNumber(4);
+});
+
+const five = document.querySelector("#five");
+five.addEventListener('click', () => {
+    storeNumber(5);
+});
+
+const six = document.querySelector("#six");
+six.addEventListener('click', () => {
+    storeNumber(6);
+});
+
+const seven = document.querySelector("#seven");
+seven.addEventListener('click', () => {
+    storeNumber(7);
+});
+
+const eight = document.querySelector("#eight");
+eight.addEventListener('click', () => {
+    storeNumber(8);
+});
+
+const nine = document.querySelector("#nine");
+nine.addEventListener('click', () => {
+    storeNumber(9);
+});
+
+const zero = document.querySelector("#zero");
+zero.addEventListener('click', () => {
+    storeNumber(0);
+});
+
+const sum = document.querySelector("#add");
+sum.addEventListener('click', () => {
+    storeOperator("+");
+});
+
+const substraction = document.querySelector("#substract");
+substraction.addEventListener('click', () => {
+    storeOperator("-");
+});
+
+const multiplication = document.querySelector("#multiply");
+multiplication.addEventListener('click', () => {
+    storeOperator("*");
+});
+
+const division = document.querySelector("#divide");
+division.addEventListener('click', () => {
+    storeOperator("/");
+});
+
+const equals = document.querySelector("#equals");
+//equals.addEventListener('click', () => {
+//    if (a !== null & operator !== null & b !== null) {
+//        result = operate(a, operator, b);
+//        displayText.textContent = result;
+//    }
+//});
+
+const point = document.querySelector("#point");
+
+const clear = document.querySelector("#clear");
+clear.addEventListener('click', () => {
+    operation = [];
+    number = [];
+    result = null;
+});
